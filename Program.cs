b2315ad73157ff40a26cb2183e1082d603751d17
mani1 <mani1@NANI143>
2022-07-31 20:02:18 +0530

    [Rajeswari]Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace SnakeAndLadder
+{
+    class WinningPosition
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Conditions is Repeat Till The  Player Reaches Position 100");
+            WinningPosition.CheckWinningPosition();
+        }
+    }
+}
