d610ca8fdaab4f8faacce4c14d9b6f01dc23371a
mani1 <mani1@NANI143>
2022-07-31 19:38:57 +0530

    [Rajeswari]Added program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SnakeAndLadder
+{
+    class program
+    {
+        public static string CheckOption { get; private set; }
+
+        public static void Main()
+        {
+            Console.WriteLine("CheckOption " + CheckOption);
+            CheckOption.CheckOption();
+        }
+    }
+}
