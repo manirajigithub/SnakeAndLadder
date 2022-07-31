5e34d8fb6d9a04802c13da6d1c588531689149ba
mani1 <mani1@NANI143>
2022-07-31 20:43:38 +0530

    [Rajeswari]Added Exacting Winning

---
+++ ExactWinningUC5.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadder
+{
+    public class ExactWinningPosition
+    {
+        public const int ladder = 1;
+        public const int snake = 2;
+        public static void CheckExactWinningPosition()
+        {
+            int position = 0;
+            int dieRoll;
+            Console.WriteLine("Player starts At Position");
+            Random random = new Random();
+
+            while (position < 100)
+            {
+
+                dieRoll = random.Next(1, 7);
+                int options = random.Next(0, 3);
+
+                switch (options)
+                {
+                    case ladder:
+                        Console.WriteLine("Wow!GettingLadder");
+                        position = position + dieRoll;
+                        Console.WriteLine("Position:" + position);
+                        break;
+                    case snake:
+                        Console.WriteLine("Oops!snake bit");
+                        position = position - dieRoll;
+                        Console.WriteLine("Position:" + position);
+                        if (position < 0)
+                        {
+                            position = 0;
+                            Console.WriteLine("Player Restart From Position 0:");
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("No-Play");
+                        break;
+                }
+
+                Console.WriteLine("**Present Position Is:**" + position);
+                if (position > 100)
+                {
+                    position = position - dieRoll;
+                    Console.WriteLine("Previous Position:");
+                    Console.WriteLine(position);
+                }
+
+            }
+            Console.WriteLine("Congratulations! Player Wins");
+            
+
+        }
+    }
+}
