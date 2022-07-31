65a10d8d28b32f5ddadf8c75d571b1465bedbf51
mani1 <mani1@NANI143>
2022-07-31 20:31:00 +0530

    [Rajeswari]Added program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more informationusing System;
+using System;
+namespace SnakeAndLadder
+{
+    class ExactWinningPosition
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Player Gets Exact Winning Position");
+            ExactWinningPosition.CheckExactWinningPosition();
+        }
+    }
+}
