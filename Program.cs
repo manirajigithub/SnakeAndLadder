7c7e826de89b4fed10d9d444cc73f9cc6cf5e5b1
mani1 <mani1@NANI143>
2022-07-31 14:13:09 +0530

    [Rajeswari]Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace SnakeAndLadder
+{
+    class program
+    {
+        
+        public static void Main()
+        {
+        Console.WriteLine("Welcome to game of snake and ladder");
+        
+        }
+    }
+}
