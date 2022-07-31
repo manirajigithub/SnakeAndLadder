f4c594dca963e3a08c23ac852f214b04857861f8
mani1 <mani1@NANI143>
2022-07-31 14:18:22 +0530

    [Rajeswari]Added SnakeAndLadder Start With 0

---
+++ Uc1 Start With 0.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Uc1SnakeAndLadder
+{
+    public void Start()
+    {
+        int Begin = 0;
+        Console.WriteLine("Starting position is: " + Begin);
+
+    }
+
+}
